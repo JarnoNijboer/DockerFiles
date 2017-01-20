bfd52b8013991d781edda41f0d5f0e398538c9a5
Jarno Nijboer <jnijboer@sintrum.nl>
2017-01-20 21:41:07 +0100

    WIP

--- Nginx-PHP-SqlServer/start-nginx-php.bat
+++ Nginx-PHP-SqlServer/start-nginx-php.bat
@@ -1,5 +1,4 @@
 @ECHO OFF
 ECHO Starting Nginx + PHP ...
-c:\RunHiddenConsole.exe C:\php\php-cgi.exe -b 127.0.0.1:9123
-c:\RunHiddenConsole.exe c:\nginx\nginx.exe
-EXIT
+c:\php\php-cgi.exe -b 127.0.0.1:9000 && c:\nginx\nginx.exe
+
